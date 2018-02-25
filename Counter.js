8b2cf1b4f06f1a97d796a86eb109c6c9882be9df
kindgeek <olenyuk@ucu.edu.ua>
2018-02-25 18:41:20 +0200

    add toogle to reducer with tests

--- Counter.js
+++ Counter.js
@@ -1,14 +1,30 @@
 const todos = (state = [], action) => {
-    switch (action.type){
+    switch (action.type) {
+
+
+
         case 'ADD_TODO':
             return [
                 ...state,
+                
                 {
                     id: action.id,
                     text: action.text,
                     completed: false
                 }
             ];
+        case 'TOGGLE_TODO':
+            //map to produce new array - not change previous
+            return state.map(todo => {
+                if (todo.id !== action.id) {
+                    return todo;
+                }
+                return {
+                    ...todo,
+                    completed: !todo.completed
+                };
+            });
+
         default:
             return state;
     }
@@ -32,6 +48,47 @@ const testAddTodo = () => {
         todos(stateBefore, action)).toEqual(stateAfter);
 
 };
-testAddTodo();
+
+//create an action to change state and tests this state
+
+const testtoogleTodo = () => {
+    const stateBefore = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go',
+            completed: false
+        }
+    ];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go',
+            completed: true
+        },
+
+    ];
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+    expect(
+        todos(stateBefore, action)).toEqual(stateAfter);
+
+};
+// testAddTodo();
+testtoogleTodo();
 
 console.log('All tests passed.');
