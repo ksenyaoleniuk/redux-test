f21495035ff8ee88f738cce00c786b5da01f2cef
kindgeek <olenyuk@ucu.edu.ua>
2018-02-25 16:52:19 +0200

    create todo Reducer with tests

--- Counter.js
+++ Counter.js
@@ -1,53 +1,37 @@
-const addCounter = (list) => {
-    return [...list, 0];
+const todos = (state = [], action) => {
+    switch (action.type){
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ];
+        default:
+            return state;
+    }
 };
 
-const testAddCounter = () => {
-    const listBefore = [];
-    const listAfter = [0];
-    deepFreeze(listBefore);
+const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'Learn Redux'
+    };
+    const stateAfter = [{
+        id: 0,
+        text: 'Learn Redux',
+        completed: false
+    }];
+    deepFreeze(stateBefore);
+    deepFreeze(action);
     expect(
-        addCounter(listBefore)
-    ).toEqual(listAfter);
+        todos(stateBefore, action)).toEqual(stateAfter);
+
 };
+testAddTodo();
 
-const removeCounter = (list, index) => {
-    return [
-        ...list.slice(0, index),
-        ...list.slice(index + 1)
-    ];
-};
-
-const testRemoveCounter = () => {
-    const listBefore = [0, 10, 20];
-    const listAfter = [0, 20];
-
-    expect(
-        removeCounter(listBefore, 1)
-    ).toEqual(listAfter);
-};
-
-
-const incrementCounter = (list, index) => {
-    return [
-        ...list.slice(0, index),
-        list[index] + 1,
-        ...list.slice(index + 1)
-    ];
-};
-
-const testIncrementCounter = () => {
-    const listBefore = [0, 10, 20];
-    const listAfter = [0, 11, 20];
-
-    deepFreeze(listBefore);
-
-    expect(
-        incrementCounter(listBefore, 1)
-    ).toEqual(listAfter);
-};
-
-testAddCounter();
-testRemoveCounter();
-testIncrementCounter();
-console.log('All tests passed');
+console.log('All tests passed.');
