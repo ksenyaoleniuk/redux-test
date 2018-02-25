62423639b2be4443f2ffaa3e1d5a0ef1f50df0ce
kindgeek <olenyuk@ucu.edu.ua>
2018-02-25 19:56:13 +0200

    add toggle todo functionality

--- TodoApp.js
+++ TodoApp.js
@@ -1,4 +1,4 @@
-const { Component } = React;
+const {Component} = React;
 
 let nextTodoId = 0;
 
@@ -7,7 +7,8 @@ class TodoApp extends Component {
         return (
             <div>
                 <input ref={node => {
-                this.input = node;}}/>
+                    this.input = node;
+                }}/>
                 <button onClick={() => {
                     store.dispatch({
                         type: 'ADD_TODO',
@@ -20,83 +21,95 @@ class TodoApp extends Component {
                 </button>
                 <ul>
                     {this.props.todos.map(todo =>
-                        <li key={todo.id}>
+                        <li
+                            key={todo.id}
+                            onClick={() => {
+                                store.dispatch({
+                                        type: 'TOGGLE_TODO',
+                                        id: todo.id
+                                    }
+                                );
+                            }}
+                            style={{textDecoration:
+                                    todo.completed ? 'line-through': 'none'
+                            }}
+                                >
                             {todo.text}
-                        </li>
-                    )}
-                </ul>
-            </div>
-        )
-    };
-}
+                                </li>
+                                )}
+                        </ul>
+                        </div>
+                        )
+                    };
+                    }
 
-const todo = (state, action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return {
-                id: action.id,
-                text: action.text,
-                completed: false
-            };
-        case 'TOGGLE_TODO':
-            if (state.id !== action.id) {
-                return state;
-            }
+                    const todo = (state, action) => {
+                    switch (action.type) {
+                    case 'ADD_TODO':
+                    return {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                };
+                    case 'TOGGLE_TODO':
+                    if (state.id !== action.id) {
+                    return state;
+                }
 
-            return {
-                ...state,
-                completed: !state.completed
-            };
-        default:
-            return state;
-    }
-};
+                    return {
+                    ...state,
+                    completed: !state.completed
+                };
+                    default:
+                    return state;
+                }
+                };
 
-const todos = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                todo(undefined, action)
-            ];
-        case 'TOGGLE_TODO':
-            return state.map(t => todo(t, action));
-        default:
-            return state;
-    }
-};
+                    const todos = (state = [], action) => {
+                    switch (action.type) {
+                    case 'ADD_TODO':
+                    return [
+                    ...state,
+                    todo(undefined, action)
+                    ];
+                    case 'TOGGLE_TODO':
+                    return state.map(t => todo(t, action));
+                    default:
+                    return state;
+                }
+                };
 
-// can add more info to the object without changing existing reducer
-const visibilityFilter = (
-    state = 'SHOW_ALL',
-    action
-) => {
-    switch (action.type) {
-        case 'SET_VISIBILITY_FILTER':
-            return action.filter;
-        default:
-            return state;
-    }
-};
-const { combineReducers } = Redux;
+                    // can add more info to the object without changing existing reducer
+                    const visibilityFilter = (
+                    state = 'SHOW_ALL',
+                    action
+                    ) => {
+                    switch (action.type) {
+                    case 'SET_VISIBILITY_FILTER':
+                    return action.filter;
+                    default:
+                    return state;
+                }
+                };
+                    const {combineReducers} = Redux;
 
-const todoApp = combineReducers({
-    todos,
-    visibilityFilter
-});
+                    const todoApp = combineReducers({
+                    todos,
+                    visibilityFilter
+                });
 
-const { createStore } = Redux;
-const store = createStore(todoApp);
+                    const {createStore} = Redux;
+                    const store = createStore(todoApp);
 
-const render = () => {
-    ReactDOM.render(
-        <TodoApp todos={store.getState().todos}/>,
-        document.getElementById('root')
-    )
-};
+                    const render = () => {
+                    ReactDOM.render(
+                        <TodoApp todos={store.getState().todos}/>,
+                        document.getElementById('root')
+                    )
+                };
 
-store.subscribe(render);
-render();
+                    store.subscribe(render);
+                    render();
 
 
 
