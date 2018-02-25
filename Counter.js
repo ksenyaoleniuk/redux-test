5a051e9d985268a2fd450d85b95505b88479602c
kindgeek <olenyuk@ucu.edu.ua>
2018-02-25 15:57:51 +0200

    manage with array to make pushing, incrementing, removing immutable

--- Counter.js
+++ Counter.js
@@ -1,41 +1,53 @@
-const counter = (state = 0, action)=> {
-
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        default:
-            return state;
-    }
-
-}
-
-const Counter = ({value, onIncrement, onDecrement,}) => {
-    return (
-        <div>
-            <h1>
-                {value}
-            </h1>
-            <button onClick={onIncrement}>+</button>
-            <button onClick={onDecrement}>-</button>
-        </div>
-    )
+const addCounter = (list) => {
+    return [...list, 0];
 };
 
-// updating dom using react
-const {createStore} = Redux;
-const store = createStore(counter);
-const render = () => {
-    ReactDOM.render(
-        <Counter value={store.getState()}
-                 onIncrement={() => store.dispatch({type: 'INCREMENT'})}
-                 onDecrement={() => store.dispatch({type: 'DECREMENT'})}
-        />,
-        document.getElementById("root")
-    )
+const testAddCounter = () => {
+    const listBefore = [];
+    const listAfter = [0];
+    deepFreeze(listBefore);
+    expect(
+        addCounter(listBefore)
+    ).toEqual(listAfter);
 };
-store.subscribe(render);
-render();
+
+const removeCounter = (list, index) => {
+    return [
+        ...list.slice(0, index),
+        ...list.slice(index + 1)
+    ];
+};
+
+const testRemoveCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 20];
+
+    expect(
+        removeCounter(listBefore, 1)
+    ).toEqual(listAfter);
+};
 
 
+const incrementCounter = (list, index) => {
+    return [
+        ...list.slice(0, index),
+        list[index] + 1,
+        ...list.slice(index + 1)
+    ];
+};
+
+const testIncrementCounter = () => {
+    const listBefore = [0, 10, 20];
+    const listAfter = [0, 11, 20];
+
+    deepFreeze(listBefore);
+
+    expect(
+        incrementCounter(listBefore, 1)
+    ).toEqual(listAfter);
+};
+
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+console.log('All tests passed');
