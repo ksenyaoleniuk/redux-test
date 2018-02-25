222375168eaaf9e0074d04d60ca3cc664214b63b
kindgeek <olenyuk@ucu.edu.ua>
2018-02-25 20:20:36 +0200

    code refactoring, create separate components Todo and TodoList

--- TodoApp.js
+++ TodoApp.js
@@ -32,6 +32,32 @@ const FilterLink = ({filter, children}) => {
 
 let nextTodoId = 0;
 
+
+const Todo = ({onClick, completed, text}) => (
+    <li
+        onClick={onClick}
+        style={{
+            textDecoration:
+                completed ?
+                    'line-through' :
+                    'none'
+        }}>
+        {text}
+    </li>
+);
+
+const TodoList = ({todos, onTodoClick}) => (
+    <ul>
+        {todos.map(todo =>
+            <Todo
+                key={todo.id}
+                {...todo}
+                onClick={() => onTodoClick(todo.id)}
+            />
+        )}
+    </ul>
+)
+
 class TodoApp extends Component {
     render() {
         const visibleTodos = getVisibleTodos(this.props.todos, this.props.visibilityFilter)
@@ -50,26 +76,18 @@ class TodoApp extends Component {
                 }}>
                     Add Todo
                 </button>
-                <ul>
-                    {visibleTodos.map(todo =>
-                        <li
-                            key={todo.id}
-                            onClick={() => {
-                                store.dispatch({
-                                        type: 'TOGGLE_TODO',
-                                        id: todo.id
-                                    }
-                                );
-                            }}
-                            style={{
-                                textDecoration:
-                                    todo.completed ? 'line-through' : 'none'
-                            }}
-                        >
-                            {todo.text}
-                        </li>
-                    )}
-                </ul>
+                <TodoList
+                    todos={visibleTodos}
+                    onTodoClick={id =>
+                        store.dispatch(
+                            {
+                                type: 'TOGGLE_TODO',
+                                id
+                            }
+                        )
+                    }
+                />
+
                 <p>
                     Show:
                     {' '}
