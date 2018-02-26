1d3773f60d731683d28fd7c756371c84230f5264
kindgeek <olenyuk@ucu.edu.ua>
2018-02-26 23:24:08 +0200

    fix bug

--- TodoApp.js
+++ TodoApp.js
@@ -243,7 +243,7 @@ ReactDOM.render(
     <Provider store={createStore(todoApp)}>
         <TodoApp />
     </Provider>,
-    ,document.getElementById('root'));
+    document.getElementById('root'));
 
 // store.subscribe(render);
 // render();
