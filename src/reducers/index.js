f6ca6dff53f9a9d9b314293e8057924cd88b3451
kindgeek <olenyuk@ucu.edu.ua>
2018-03-05 20:41:28 +0200

    Colocating Selectors with reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,12 @@
 //done
 const {combineReducers} = Redux;
-import todos from './todos'
+import todos , * as fromTodos from './todos'
+// import todos  from './todos'
+
 const todoApp = combineReducers({
     todos,
 });
 export default todoApp
+
+export const getVisibleTodos = (state, filter) =>
+    fromTodos.getVisibleTodos(state.todos, filter);
--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -22,3 +22,23 @@ const todos = (state = [], action) => {
 }
 
 export default todos
+
+export const getVisibleTodos = (state, filter) => {
+    switch (filter) {
+        case 'all':
+            console.log(filter,'all:',state);
+            return state;
+        case 'completed':
+            console.log('comp:',state);
+            return state.filter(t => t.completed);
+
+        case 'active':
+            console.log('active:',state);
+
+            return state.filter(
+                t => !t.completed
+            );
+        default:
+            throw new  Error(`Unknown filter: ${filter}.`);
+    }
+};
